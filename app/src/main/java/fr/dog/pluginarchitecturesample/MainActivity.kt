f9f3376427d703373fcc16fd48b61cb45ef6f298
tboutin <tboutin@ippon.fr>
2022-07-28 15:56:15 +0200

    feat: use compose instead of legacy Android UI Kit

---
+++ app/src/main/java/fr/dog/pluginarchitecturesample/MainActivity.kt
@@ -0,0 +1,16 @@
+package fr.dog.pluginarchitecturesample
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import fr.dog.pluginarchitecturesample.databinding.ActivityItemDetailBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
