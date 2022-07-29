e80e3d75e1ab1d752894c8bfbf7d2df85beeee67
tboutin <tboutin@ippon.fr>
2022-07-29 10:20:29 +0200

    feat: add detail module

---
+++ app/src/main/java/fr/dog/pluginarchitecturesample/HomeScreen.kt
@@ -0,0 +1,9 @@
+package fr.dog.pluginarchitecturesample
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen() {
+    Text(text = "Bonjour")
+}
--- app/src/main/java/fr/dog/pluginarchitecturesample/MainActivity.kt
+++ app/src/main/java/fr/dog/pluginarchitecturesample/MainActivity.kt
@@ -1,16 +1,28 @@
 package fr.dog.pluginarchitecturesample
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import fr.dog.pluginarchitecturesample.databinding.ActivityItemDetailBinding
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContent {
+            MainScreen(rememberNavController())
+        }
+    }
+}
+
+@Composable
+fun MainScreen(navController: NavHostController) {
+    NavHost(navController = navController, startDestination = "home") {
+        composable(route = "home") {
+            HomeScreen()
+        }
     }
 }
